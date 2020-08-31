c1d99ac917ee6dd496df9270d6b5d6aa3c9d425d
Takehiro Takahashi <taka.mc.0509@gmail.com>
2020-08-31 16:00:07 +0900

    Initial Commit

---
+++ CounterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CounterApp
+//
+//  Created by 高橋岳宏 on 2020/08/31.
+//  Copyright © 2020 高橋岳宏. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CounterAppTests/CounterAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CounterAppTests.swift
+//  CounterAppTests
+//
+//  Created by 高橋岳宏 on 2020/08/31.
+//  Copyright © 2020 高橋岳宏. All rights reserved.
+//
+
+import XCTest
+@testable import CounterApp
+
+class CounterAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
